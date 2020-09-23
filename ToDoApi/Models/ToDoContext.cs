9c3c3472dd19a0ffff4fd5e7f19ae3b5a25e5e3d
Private <Private@SHAFWANTIM-PC>
2020-09-23 15:01:10 +0200

    a

---
+++ ToDoApi/Models/ToDoContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi.Models
+{
+    using Microsoft.EntityFrameworkCore;
+    using ToDoApi.Models;
+
+    namespace TodoApi.Models
+    {
+        public class TodoContext : DbContext
+        {
+            public TodoContext(DbContextOptions<TodoContext> options)
+                : base(options)
+            {
+            }
+
+            public DbSet<TodoItem> TodoItems { get; set; }
+        }
+    }
+}
